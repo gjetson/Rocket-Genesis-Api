e0f78926e2cd261a572a694c8fd713179961d992
Del Barnwell <dbarnwel@google.com>
2023-02-21 12:30:56 -0500

    initial setup

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 // Initial dependencies and definitions
-require('dotenv').config();
 const Express = require('express');
+const MongoManager = require('./src/shared/db/mongodb/mongo-manager')
+
+require('dotenv').config()
+console.log(process.env)
+
 const app = Express();
 const port = process.env.PORT || 3004;
 
 // Import routes
 const HealthRoutes = require('./src/routes/health.routes');
 
-
 app.use(Express.json());
 
 HealthRoutes.registerHealthRoutes(app);
+MongoManager.openMongoConnection(process.env.MONGO_URI);
 
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`)
---
+++ src/shared/db/mongodb/mongo-manager.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+//require('dotenv').config()
+
+
+const openMongoConnection = (uri) => {
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', () => {
+        console.log("connected to MongoDB");
+    });
+    mongoose.connect(uri);
+    console.log(`uri: ${uri}`)
+};
+
+mongoose.set('strictQuery', true)
+
+module.exports = { openMongoConnection };
